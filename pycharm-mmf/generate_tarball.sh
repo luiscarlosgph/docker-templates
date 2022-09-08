a11cc9611f1951ad43c4112ad7f95a703dd4b541
Luis C. Garcia Peraza Herrera <luiscarlos.gph@gmail.com>
2022-09-08 01:05:53 +0100

    Automatic listing of files in generate_tarball.sh of pycharf-mmf.

--- pycharm-mmf/generate_tarball.sh
+++ pycharm-mmf/generate_tarball.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
-tar czf cuda_pyenv_pytorch_pycharm_mmf.tar.gz -T tarball_files.txt
+IMAGE_NAME=`basename "$PWD"`
+
+FNAME='tarball_files.txt'
+DNAME='files'
+touch $FNAME
+
+# Add Dockerfile
+echo 'Dockerfile' >> $FNAME
+
+# Add all the files in the folder 'files'
+for entry in "$DNAME"/*
+do
+  echo "$entry" >> $FNAME
+done
+
+# Create tarball
+tar czf $IMAGE_NAME.tar.gz -T $FNAME
+
+# Cleanup temporary file
+rm -f $FNAME
